85d8484c0ef8b7375b586f88c385698cd71295c1
krishnaraj199 <kishorekrish255@gmail.com>
2024-02-16 13:51:13 +0530

    added a new file

---
+++ q1.js
@@ -0,0 +1,4 @@
+/* // The difference between HTTP1 and HTTP2 is mainly in how they transfer data between the client and the server. 
+HTTP1 uses plain text messages that are sent one at a time over a single connection.
+while HTTP2 uses binary frames that are multiplexed over a single connection. 
+HTTP2 also has features like header compression, faster, more efficient, and more secure than HTTP1 */
---
+++ q2.js
@@ -0,0 +1,6 @@
+/* An object in JavaScript is a collection of properties, where each property has a name or key and a value.
+The value can be a primitive data type, such as a number or a string, or another object, such as an array or a function.
+Objects are used to store and manipulate complex data in JavaScript.
+Objects can be created using different ways, such as object literals, constructors, or the Object.create() method.
+An Each object has an internal link to another object, called its prototype.
+The prototype object can also have its own properties and methods, which are inherited by the objects that link to it. */
